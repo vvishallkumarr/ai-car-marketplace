411dff29a4d3008935a676408307eccc4a4407b7
Vishal Kumar <iamvishalumar30@gmail.com>
2025-06-07 23:54:15 +0530

    latest
    latest

---
+++ next.config.mjs
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsHmrCache: false, // defaults to true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ymqpkygmownybanldbpq.supabase.co",
+      },
+    ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/embed",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-src 'self' https://roadsidecoder.created.app;",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
